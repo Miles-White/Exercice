ec1a1fb8fe709465aa1e4c9b86595c98b15f54f2
Miles-White <131509706+Miles-White@users.noreply.github.com>
2024-06-17 23:33:12 +0200

    Update Fonction1.c

--- Fonction1.c
+++ Fonction1.c
@@ -1,37 +1,46 @@
-#include <stdio.h>
-#include <unistd.h>
-#include "O:\INFO1\Bib_INFO1.h"
-#include <math.h>
+#include <stdio.h>      // Bibliothèque standard pour les fonctions d'entrée-sortie comme printf et scanf
+#include <unistd.h>     // API POSIX pour la fonction sleep
+#include "O:\INFO1\Bib_INFO1.h" // Inclusion de la bibliothèque personnalisée pour la gestion de la console
+#include <math.h>       // Bibliothèque mathématique
 
-
-int main(int argc, char const*argy[])
+int main(int argc, char const *argy[])
 {
-    int A,B,C,D,E,somme;
-    gotoXY(5,5);
+    // Déclaration des variables pour stocker les entiers et leur somme
+    int A, B, C, D, E, somme;
+    
+    // Utilisation de gotoXY pour positionner le curseur et demander la saisie du premier entier
+    gotoXY(5, 5);
     printf("Veuillez-saisir l'entier numero 1: ");
     scanf("%d", &A);
     
-    gotoXY(5,6);
+    // Demande la saisie du deuxième entier
+    gotoXY(5, 6);
     printf("Veuillez-saisir l'entier numero 2: ");
     scanf("%d", &B);
 
-    gotoXY(5,7);
+    // Demande la saisie du troisième entier
+    gotoXY(5, 7);
     printf("Veuillez-saisir l'entier numero 3: ");
     scanf("%d", &C);
 
-    gotoXY(5,8);
+    // Demande la saisie du quatrième entier
+    gotoXY(5, 8);
     printf("Veuillez-saisir l'entier numero 4: ");
     scanf("%d", &D);
 
-    gotoXY(5,9);
+    // Demande la saisie du cinquième entier
+    gotoXY(5, 9);
     printf("Veuillez-saisir l'entier numero 5: ");
     scanf("%d", &E);
 
+    // Calcul de la somme des cinq entiers
     somme = A + B + C + D + E;
 
-    gotoXY(5,11);
+    // Affiche la somme des cinq entiers
+    gotoXY(5, 11);
     printf("La somme des 5 entiers = %d", somme);
 
+    // Pause de 5 secondes avant de terminer le programme
     Sleep(5000);
-    return 0;
+    return 0; // Retourne 0 pour indiquer que le programme s'est terminé correctement
 }
